6259d277dbe0d70398c23c1272e0561b27c14b54
LaysDragon <laysdra7265@gmail.com>
2017-12-15 14:03:11 +0800

    網頁輸出自動換行<br>

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash, jsonify
+    abort, render_template, flash, jsonify,Markup
 from processlib.Scanner import Scanner
 from processlib.Parser import Parser
 import processlib.Tools as Tools
@@ -23,10 +23,11 @@ def index():
 @app.route('/parse', methods=['POST'])
 def add_entry():
     c0_code = request.form['text']
-    parser = Parser (Scanner(c0_code))
-    parser_res = Tools.dfs_dump(parser.parsed_tree)
-    flash(parser_res)
+    parser = Parser(Scanner(c0_code))
+    parser_res = Tools.dfs_dump(parser.parsed_tree).replace('\n', '<br>')
+    flash(Markup(parser_res))
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
